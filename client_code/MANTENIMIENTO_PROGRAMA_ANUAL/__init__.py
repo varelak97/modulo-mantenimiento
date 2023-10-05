e0c96eb7114798cb1e904527c82e4711d7440fb3
erp@ensel.org <erp@ensel.org>
2023-10-05 17:32:53 +0000

    Created superSQL.js and test.js; Edited standard-page.html and MANTENIMIENTO_PROGRAMA_ANUAL

--- client_code/MANTENIMIENTO_PROGRAMA_ANUAL/__init__.py
+++ client_code/MANTENIMIENTO_PROGRAMA_ANUAL/__init__.py
@@ -1,5 +1,6 @@
 from ._anvil_designer import MANTENIMIENTO_PROGRAMA_ANUALTemplate
 from anvil import *
+import anvil.js
 
 class MANTENIMIENTO_PROGRAMA_ANUAL(MANTENIMIENTO_PROGRAMA_ANUALTemplate):
   #################################### DEFINICION DE VARIABLES ####################################
@@ -11,7 +12,11 @@ class MANTENIMIENTO_PROGRAMA_ANUAL(MANTENIMIENTO_PROGRAMA_ANUALTemplate):
     self.init_components(**properties)
 
   def button_nuevo_click(self, **event_args):
-    self.datos['clave_form'] = 'MANTENIMIENTO_VERIFICACION_MTTO_PREVENTIVO'
+    """self.datos['clave_form'] = 'MANTENIMIENTO_VERIFICACION_MTTO_PREVENTIVO'
     self.datos['modo'] = 'nuevo'
-    self.parent.raise_event('x-actualizar_form_activo', datos=self.datos)
+    self.parent.raise_event('x-actualizar_form_activo', datos=self.datos)"""
+    lista = [["nombre","edad"],["ALFREDO VARELA",33],["FERNANDO CORTES",31],["MARIO GONZALES",45]]
+    datos = anvil.js.call('SUPERSQL',f"SELECT * FROM ? WHERE edad < 40',{lista}")
+    print(datos)
+    
 
