94663ba33b7953f478b1c4d9b8435a8a89f6650c
erp@ensel.org <erp@ensel.org>
2023-10-10 22:06:47 +0000

    Created MANTENIMIENTO_PREVENTIVO.FORMULARIO_EQUIPOS_ATMAXX.RowTemplate3; Edited settings and MANTENIMIENTO_PREVENTIVO.FORMULARIO_EQUIPOS_ATMAXX

--- client_code/MANTENIMIENTO_PREVENTIVO/FORMULARIO_EQUIPOS_ATMAXX/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO/FORMULARIO_EQUIPOS_ATMAXX/__init__.py
@@ -3,9 +3,17 @@ from anvil import *
 
 class FORMULARIO_EQUIPOS_ATMAXX(FORMULARIO_EQUIPOS_ATMAXXTemplate):
   datos = {}
-  def __init__(self, datos, **properties):
+  items_actividades_maquinas = [
+    {"id":1,"actividad":"DESTAPAR Y LIMPIAR INTERIOR DEL EQUIPO"},
+    {"id":2,"actividad":"DRENAR CONTENEDOR DE ACEITE DEL SISTEMA NEUMÁTICO"},
+    {"id":3,"actividad":"LIMPIAR SILENCIADORES CON AGUA A PRESIÓN"},
+    {"id":4,"actividad":"actividad fdre"}
+  ]
+  def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.datos = datos
-
+    #self.datos = datos
+    self.repeating_panel_1.items = self.items
+    
+    
     # Any code you write here will run before the form opens.
