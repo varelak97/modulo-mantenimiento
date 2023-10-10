edea937f8f0274c4d327295ea4ad9bf68838c628
erp@ensel.org <erp@ensel.org>
2023-10-10 23:01:56 +0000

    Edited MANTENIMIENTO_PREVENTIVO.FORMULARIO_EQUIPOS_ATMAXX.RowTemplate3 and MANTENIMIENTO_PREVENTIVO.FORMULARIO_EQUIPOS_ATMAXX

--- client_code/MANTENIMIENTO_PREVENTIVO/FORMULARIO_EQUIPOS_ATMAXX/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO/FORMULARIO_EQUIPOS_ATMAXX/__init__.py
@@ -7,7 +7,17 @@ class FORMULARIO_EQUIPOS_ATMAXX(FORMULARIO_EQUIPOS_ATMAXXTemplate):
     {"id":1,"actividad":"DESTAPAR Y LIMPIAR INTERIOR DEL EQUIPO"},
     {"id":2,"actividad":"DRENAR CONTENEDOR DE ACEITE DEL SISTEMA NEUMÁTICO"},
     {"id":3,"actividad":"LIMPIAR SILENCIADORES CON AGUA A PRESIÓN"},
-    {"id":4,"actividad":"actividad fdre"}
+    {"id":4,"actividad":"ENGRASAR CHUMACERAS Y RIELES DE DESPLAZAMIENTO"},
+    {"id":5,"actividad":"REVISAR EL NIVEL DE ACEITE DE LA CAJA REDUCTORA"},
+    {"id":6,"actividad":"ENGRASAR CHUMACERAS Y RIELES DE DESPLAZAMIENTO"},
+    {"id":7,"actividad":"REVISAR EL NIVEL DE ACEITE DE LA CAJA REDUCTORA"},
+    {"id":8,"actividad":"ENGRASAR TORNILLOS Y RIELES DE DESPLAZAMIENTO"},
+    {"id":9,"actividad":"REVISAR TORNILLERÍA: APRETAR O REEMPLAZAR SI ES NECESARIO"},
+    {"id":10,"actividad":"REVISAR QUE NO EXISTAN FUGAS DE AIRE DEL SISTEMA NEUMÁTICO"},
+    {"id":11,"actividad":"REVISAR CONEXIONES EN EL PANEL DE CONTROL"},
+    {"id":12,"actividad":"LIMPIAR PANEL DE CONTROL"},
+    {"id":13,"actividad":"VERIFICAR QUE FUNCIONEN PAROS DE EMERGENCIA"},
+    {"id":14,"actividad":"REVISAR QUE INDICADOR DE BATERÍA EN PLC NO ESTÉ ENCENDIDO"}
   ]
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
