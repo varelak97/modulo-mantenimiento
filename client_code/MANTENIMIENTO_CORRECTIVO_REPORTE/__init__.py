eafd47227a741105fbddd9c6166aa1ea94d99783
erp@ensel.org <erp@ensel.org>
2023-10-11 21:50:23 +0000

    Created MANTENIMIENTO_CORRECTIVO_REPORTE

---
+++ client_code/MANTENIMIENTO_CORRECTIVO_REPORTE/__init__.py
@@ -0,0 +1,9 @@
+from ._anvil_designer import MANTENIMIENTO_CORRECTIVO_REPORTETemplate
+from anvil import *
+
+class MANTENIMIENTO_CORRECTIVO_REPORTE(MANTENIMIENTO_CORRECTIVO_REPORTETemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
