ce15085ed741f0e081e065e47eb0a802466f09c8
erp@ensel.org <erp@ensel.org>
2023-10-12 13:17:23 +0000

    Edited MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplate1

--- client_code/MANTENIMIENTO_PROGRAMA_ANUAL/RowTemplate1/__init__.py
+++ client_code/MANTENIMIENTO_PROGRAMA_ANUAL/RowTemplate1/__init__.py
@@ -7,3 +7,8 @@ class RowTemplate1(RowTemplate1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
