1721acb8f3d8ce59b6c456608244c80255875348
erp@ensel.org <erp@ensel.org>
2023-10-16 20:13:18 +0000

    Created MANTENIMIENTO_PREVENTIVO_REGISTROS.RowTemplateEditar; Edited MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplateDias and MANTENIMIENTO_PREVENTIVO_REGISTROS; Deleted MANTENIMIENTO_PREVENTIVO_REGISTROS.RowTemplate2 and MANTENIMIENTO_PREVENTIVO_REGISTROS.RowTemplate4

--- client_code/MANTENIMIENTO_PREVENTIVO_REGISTROS/RowTemplateEditar/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_REGISTROS/RowTemplateEditar/__init__.py
@@ -1,11 +1,15 @@
-from ._anvil_designer import RowTemplate4Template
+from ._anvil_designer import RowTemplateEditarTemplate
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 
-class RowTemplate4(RowTemplate4Template):
+class RowTemplateEditar(RowTemplateEditarTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def button_editar_click(self, **event_args):
+    print(self.tag)
+
