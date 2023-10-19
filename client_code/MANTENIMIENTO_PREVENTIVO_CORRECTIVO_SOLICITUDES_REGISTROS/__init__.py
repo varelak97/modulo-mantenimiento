1e9e185eef211dc5908f9e7537bebb31a52c5acd
erp@ensel.org <erp@ensel.org>
2023-10-19 18:26:37 +0000

    Created MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS.RowTemplateEditar; Edited settings, MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS, A_main and MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES

--- client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS/__init__.py
@@ -4,8 +4,8 @@ import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 
 class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS(MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROSTemplate):
-  def __init__(self, **properties):
+  datos = {}
+  def __init__(self, datos, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
+    self.datos = datos
