a9a42c57a1679d3da60cfdbbe807bec36ef1b74c
erp@ensel.org <erp@ensel.org>
2023-10-19 21:19:46 +0000

    Edited settings, MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES_REGISTROS and MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES

--- client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
@@ -4,8 +4,116 @@ import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 
 class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES(MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDESTemplate):
+  #################################### DEFINICION DE VARIABLES ####################################
   datos = {}
-  def __init__(self, datos, **properties):
-    # Set Form properties and Data Bindings.
+  lista_areas = [
+    "IMPRESIÓN",
+    "SUAJE",
+    "MANUALES",
+    "LÁSER",
+    "CALIDAD",
+    "REVELADO",
+    "ENSAMBLE",
+    "ALMACÉN MP"
+  ]
+  lista_equipos = [
+    ("ATMA 57",{"EQUIPO":"ATMA 57","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 71",{"EQUIPO":"ATMA 71","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 70",{"EQUIPO":"ATMA 70","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 45",{"EQUIPO":"ATMA 45","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 710",{"EQUIPO":"ATMA 710","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 80",{"EQUIPO":"ATMA 80","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("HORNO 1",{"EQUIPO":"HORNO 1","AREA":"IMPRESIÓN","FRECUENCIA":["SEMANAL","MENSUAL","SEMESTRAL"]}),
+    ("HORNO 2",{"EQUIPO":"HORNO 2","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
+    ("HORNO 3",{"EQUIPO":"HORNO 3","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
+    ("HORNO 4",{"EQUIPO":"HORNO 4","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
+    ("HORNO 5",{"EQUIPO":"HORNO 5","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
+    ("IMPRESORA MIMAKI",{"EQUIPO":"IMPRESORA MIMAKI","AREA":"IMPRESIÓN","FRECUENCIA":["MENSUAL"]}),
+    ("IMPRESORA OFFSET",{"EQUIPO":"IMPRESORA OFFSET","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
+    ("SPS",{"EQUIPO":"SPS","AREA":"IMPRESIÓN","FRECUENCIA":["MENSUAL"]}),
+    ("SUAJADORA 1",{"EQUIPO":"SUAJADORA 1","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("SUAJADORA 2",{"EQUIPO":"SUAJADORA 2","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("SUAJADORA 3",{"EQUIPO":"SUAJADORA 3","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("SUAJADORA 4",{"EQUIPO":"SUAJADORA 4","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("EMBOSADORA",{"EQUIPO":"EMBOSADORA","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("LÁSER V-460",{"EQUIPO":"LÁSER V-460","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
+    ("LÁSER M-300",{"EQUIPO":"LÁSER M-300","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
+    ("LÁSER VLS-360",{"EQUIPO":"LÁSER VLS-360","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
+    ("MESA DE COORDENADAS X-Y",{"EQUIPO":"MESA DE COORDENADAS X-Y","AREA":"CALIDAD","FRECUENCIA":["TRIMESTRAL"]}),
+    ("PROBADOR ELÉCTRICO 2 (CC015)",{"EQUIPO":"PROBADOR ELÉCTRICO 2 (CC015)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
+    ("PROBADOR ELÉCTRICO 3 (C0025)",{"EQUIPO":"PROBADOR ELÉCTRICO 3 (C0025)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
+    ("PROBADOR ELÉCTRICO 4 (C0028)",{"EQUIPO":"PROBADOR ELÉCTRICO 4 (C0028)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
+    ("INSOLADORA",{"EQUIPO":"INSOLADORA","AREA":"REVELADO","FRECUENCIA":["TRIMESTRAL"]}),
+    ("AFILADOR DE RASEROS",{"EQUIPO":"AFILADOR DE RASEROS","AREA":"REVELADO","FRECUENCIA":["TRIMESTRAL"]}),
+    ("LAMINADORA 1",{"EQUIPO":"LAMINADORA 1","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
+    ("LAMINADORA 2",{"EQUIPO":"LAMINADORA 2","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
+    ("LAMINADORA 3",{"EQUIPO":"LAMINADOR 3","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
+    ("PICK&PLACE 2",{"EQUIPO":"PICK&PLACE 2","AREA":"ENSAMBLE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("TROQUELADORA MANUAL",{"EQUIPO":"TROQUELADORA MANUAL","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
+    ("DISPENSADORES",{"EQUIPO":"DISPENSADORES","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
+    ("PICK&PLACE 3",{"EQUIPO":"PICK&PLACE 3","AREA":"ENSAMBLE","FRECUENCIA":["TRIMESTRAL"]}),
+    ("GUILLOTINA 1",{"EQUIPO":"GUILLOTINA 1","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
+    ("GUILLOTINA 2",{"EQUIPO":"GUILLOTINA 2","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
+    ("GUILLOTINA 3",{"EQUIPO":"GUILLOTINA 3","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
+    ("HOJEADORA",{"EQUIPO":"HOJEADORA","AREA":"ALMACÉN MP","FRECUENCIA":["TRIMESTRAL"]}),
+    ("EMBOLSADORA",{"EQUIPO":"EMBOLSADORA","AREA":"MANUALES","FRECUENCIA":["TRIMESTRAL"]}),
+  ]
+  def __init__(self, **properties):
     self.init_components(**properties)
-    self.datos = datos
+    ######################## CARGA DE DATOS E INICIALIZACION DE VARIABLES #########################
+    #self.datos = datos
+    self.drop_down_area.items = self.lista_areas
+
+  #################################### FUNCIONES PERSONALIZADS ####################################
+  def validar_campos(self):
+    dict_solicitud = {}
+    status_validacion = True
+    if self.text_box_nombre.text == "": 
+      status_validacion = False 
+    else: 
+      dict_solicitud['persona_reporta'] = self.text_box_nombre.text 
+    if self.date_picker_fecha_solicitud.date == None: 
+      status_validacion = False 
+    else: 
+      dict_solicitud['fecha_solicitud'] = self.date_picker_fecha_solicitud.date 
+    if  
+      status_validacion = False 
+    else: 
+      dict_solicitud['persona_reporta'] = self.text_box_nombre.text 
+  
+  ############################################ EVENTOS ############################################
+
+  def drop_down_area_change(self, **event_args):
+    area_seleccionada = self.drop_down_area.selected_value
+    if area_seleccionada != None:
+      equipos_area = []
+      for item in self.lista_equipos:
+        if item[1]["AREA"] == area_seleccionada:
+          equipos_area.append(item)
+      self.drop_down_equipo.items = equipos_area
+      self.outlined_card_equipo.visible = True
+      self.label_titulo_area.text = area_seleccionada
+    else:
+      self.outlined_card_equipo.visible = False
+
+  def button_enviar_click(self, **event_args):
+    if self.validar_campos():
+      alert("aqui se guardara la info")
+    else:
+      alert(title="ERROR!", content="Faltan campos por llenar!")
+
+  def drop_down_equipo_change(self, **event_args):
+    if self.drop_down_equipo.selected_value != None:
+      self.text_area_anomalia.enabled = True
+    else:
+      self.text_area_anomalia.enabled = False
+
+  def text_area_anomalia_change(self, **event_args):
+    if self.text_area_anomalia.text == None or self.text_area_anomalia.text == "":
+      self.button_enviar.enabled = False
+    else:
+      self.button_enviar.enabled = True
+
+
+
+
