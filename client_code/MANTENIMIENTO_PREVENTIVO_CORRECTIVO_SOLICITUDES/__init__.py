00a5bafefd8e0eca8c75e151b64d29d14f447474
erp@ensel.org <erp@ensel.org>
2023-10-20 14:32:13 +0000

    Edited MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES

--- client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
@@ -15,49 +15,50 @@ class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES(MANTENIMIENTO_PREVENTIVO_C
     "CALIDAD",
     "REVELADO",
     "ENSAMBLE",
-    "ALMACÉN MP"
+    "ALMACÉN MP",
+    "SERVICIOS GENERALES"
   ]
   lista_equipos = [
-    ("ATMA 57",{"EQUIPO":"ATMA 57","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("ATMA 71",{"EQUIPO":"ATMA 71","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("ATMA 70",{"EQUIPO":"ATMA 70","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("ATMA 45",{"EQUIPO":"ATMA 45","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("ATMA 710",{"EQUIPO":"ATMA 710","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("ATMA 80",{"EQUIPO":"ATMA 80","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("HORNO 1",{"EQUIPO":"HORNO 1","AREA":"IMPRESIÓN","FRECUENCIA":["SEMANAL","MENSUAL","SEMESTRAL"]}),
-    ("HORNO 2",{"EQUIPO":"HORNO 2","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
-    ("HORNO 3",{"EQUIPO":"HORNO 3","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
-    ("HORNO 4",{"EQUIPO":"HORNO 4","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
-    ("HORNO 5",{"EQUIPO":"HORNO 5","AREA":"IMPRESIÓN","FRECUENCIA":["SEMESTRAL"]}),
-    ("IMPRESORA MIMAKI",{"EQUIPO":"IMPRESORA MIMAKI","AREA":"IMPRESIÓN","FRECUENCIA":["MENSUAL"]}),
-    ("IMPRESORA OFFSET",{"EQUIPO":"IMPRESORA OFFSET","AREA":"IMPRESIÓN","FRECUENCIA":["TRIMESTRAL"]}),
-    ("SPS",{"EQUIPO":"SPS","AREA":"IMPRESIÓN","FRECUENCIA":["MENSUAL"]}),
-    ("SUAJADORA 1",{"EQUIPO":"SUAJADORA 1","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("SUAJADORA 2",{"EQUIPO":"SUAJADORA 2","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("SUAJADORA 3",{"EQUIPO":"SUAJADORA 3","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("SUAJADORA 4",{"EQUIPO":"SUAJADORA 4","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("EMBOSADORA",{"EQUIPO":"EMBOSADORA","AREA":"SUAJE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("LÁSER V-460",{"EQUIPO":"LÁSER V-460","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
-    ("LÁSER M-300",{"EQUIPO":"LÁSER M-300","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
-    ("LÁSER VLS-360",{"EQUIPO":"LÁSER VLS-360","AREA":"LÁSER","FRECUENCIA":["SEMANAL","MENSUAL"]}),
-    ("MESA DE COORDENADAS X-Y",{"EQUIPO":"MESA DE COORDENADAS X-Y","AREA":"CALIDAD","FRECUENCIA":["TRIMESTRAL"]}),
-    ("PROBADOR ELÉCTRICO 2 (CC015)",{"EQUIPO":"PROBADOR ELÉCTRICO 2 (CC015)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
-    ("PROBADOR ELÉCTRICO 3 (C0025)",{"EQUIPO":"PROBADOR ELÉCTRICO 3 (C0025)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
-    ("PROBADOR ELÉCTRICO 4 (C0028)",{"EQUIPO":"PROBADOR ELÉCTRICO 4 (C0028)","AREA":"CALIDAD","FRECUENCIA":["MENSUAL"]}),
-    ("INSOLADORA",{"EQUIPO":"INSOLADORA","AREA":"REVELADO","FRECUENCIA":["TRIMESTRAL"]}),
-    ("AFILADOR DE RASEROS",{"EQUIPO":"AFILADOR DE RASEROS","AREA":"REVELADO","FRECUENCIA":["TRIMESTRAL"]}),
-    ("LAMINADORA 1",{"EQUIPO":"LAMINADORA 1","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
-    ("LAMINADORA 2",{"EQUIPO":"LAMINADORA 2","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
-    ("LAMINADORA 3",{"EQUIPO":"LAMINADOR 3","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
-    ("PICK&PLACE 2",{"EQUIPO":"PICK&PLACE 2","AREA":"ENSAMBLE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("TROQUELADORA MANUAL",{"EQUIPO":"TROQUELADORA MANUAL","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
-    ("DISPENSADORES",{"EQUIPO":"DISPENSADORES","AREA":"ENSAMBLE","FRECUENCIA":["SEMESTRAL"]}),
-    ("PICK&PLACE 3",{"EQUIPO":"PICK&PLACE 3","AREA":"ENSAMBLE","FRECUENCIA":["TRIMESTRAL"]}),
-    ("GUILLOTINA 1",{"EQUIPO":"GUILLOTINA 1","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
-    ("GUILLOTINA 2",{"EQUIPO":"GUILLOTINA 2","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
-    ("GUILLOTINA 3",{"EQUIPO":"GUILLOTINA 3","AREA":"ALMACÉN MP","FRECUENCIA":["SEMESTRAL"]}),
-    ("HOJEADORA",{"EQUIPO":"HOJEADORA","AREA":"ALMACÉN MP","FRECUENCIA":["TRIMESTRAL"]}),
-    ("EMBOLSADORA",{"EQUIPO":"EMBOLSADORA","AREA":"MANUALES","FRECUENCIA":["TRIMESTRAL"]}),
+    ("ATMA 57",{"EQUIPO":"ATMA 57","AREA":"IMPRESIÓN"}),
+    ("ATMA 71",{"EQUIPO":"ATMA 71","AREA":"IMPRESIÓN"}),
+    ("ATMA 70",{"EQUIPO":"ATMA 70","AREA":"IMPRESIÓN"}),
+    ("ATMA 45",{"EQUIPO":"ATMA 45","AREA":"IMPRESIÓN"}),
+    ("ATMA 710",{"EQUIPO":"ATMA 710","AREA":"IMPRESIÓN"}),
+    ("ATMA 80",{"EQUIPO":"ATMA 80","AREA":"IMPRESIÓN"}),
+    ("HORNO 1",{"EQUIPO":"HORNO 1","AREA":"IMPRESIÓN"}),
+    ("HORNO 2",{"EQUIPO":"HORNO 2","AREA":"IMPRESIÓN"}),
+    ("HORNO 3",{"EQUIPO":"HORNO 3","AREA":"IMPRESIÓN"}),
+    ("HORNO 4",{"EQUIPO":"HORNO 4","AREA":"IMPRESIÓN"}),
+    ("HORNO 5",{"EQUIPO":"HORNO 5","AREA":"IMPRESIÓN"}),
+    ("IMPRESORA MIMAKI",{"EQUIPO":"IMPRESORA MIMAKI","AREA":"IMPRESIÓN"}),
+    ("IMPRESORA OFFSET",{"EQUIPO":"IMPRESORA OFFSET","AREA":"IMPRESIÓN"}),
+    ("SPS",{"EQUIPO":"SPS","AREA":"IMPRESIÓN"}),
+    ("SUAJADORA 1",{"EQUIPO":"SUAJADORA 1","AREA":"SUAJE"}),
+    ("SUAJADORA 2",{"EQUIPO":"SUAJADORA 2","AREA":"SUAJE"}),
+    ("SUAJADORA 3",{"EQUIPO":"SUAJADORA 3","AREA":"SUAJE"}),
+    ("SUAJADORA 4",{"EQUIPO":"SUAJADORA 4","AREA":"SUAJE"}),
+    ("EMBOSADORA",{"EQUIPO":"EMBOSADORA","AREA":"SUAJE"}),
+    ("LÁSER V-460",{"EQUIPO":"LÁSER V-460","AREA":"LÁSER"}),
+    ("LÁSER M-300",{"EQUIPO":"LÁSER M-300","AREA":"LÁSER"}),
+    ("LÁSER VLS-360",{"EQUIPO":"LÁSER VLS-360","AREA":"LÁSER"}),
+    ("MESA DE COORDENADAS X-Y",{"EQUIPO":"MESA DE COORDENADAS X-Y","AREA":"CALIDAD"}),
+    ("PROBADOR ELÉCTRICO 2 (CC015)",{"EQUIPO":"PROBADOR ELÉCTRICO 2 (CC015)","AREA":"CALIDAD"}),
+    ("PROBADOR ELÉCTRICO 3 (C0025)",{"EQUIPO":"PROBADOR ELÉCTRICO 3 (C0025)","AREA":"CALIDAD"}),
+    ("PROBADOR ELÉCTRICO 4 (C0028)",{"EQUIPO":"PROBADOR ELÉCTRICO 4 (C0028)","AREA":"CALIDAD"}),
+    ("INSOLADORA",{"EQUIPO":"INSOLADORA","AREA":"REVELADO"}),
+    ("AFILADOR DE RASEROS",{"EQUIPO":"AFILADOR DE RASEROS","AREA":"REVELADO"}),
+    ("LAMINADORA 1",{"EQUIPO":"LAMINADORA 1","AREA":"ENSAMBLE"}),
+    ("LAMINADORA 2",{"EQUIPO":"LAMINADORA 2","AREA":"ENSAMBLE"}),
+    ("LAMINADORA 3",{"EQUIPO":"LAMINADOR 3","AREA":"ENSAMBLE"}),
+    ("PICK&PLACE 2",{"EQUIPO":"PICK&PLACE 2","AREA":"ENSAMBLE"}),
+    ("TROQUELADORA MANUAL",{"EQUIPO":"TROQUELADORA MANUAL","AREA":"ENSAMBLE"}),
+    ("DISPENSADORES",{"EQUIPO":"DISPENSADORES","AREA":"ENSAMBLE"}),
+    ("PICK&PLACE 3",{"EQUIPO":"PICK&PLACE 3","AREA":"ENSAMBLE"}),
+    ("GUILLOTINA 1",{"EQUIPO":"GUILLOTINA 1","AREA":"ALMACÉN MP"}),
+    ("GUILLOTINA 2",{"EQUIPO":"GUILLOTINA 2","AREA":"ALMACÉN MP"}),
+    ("GUILLOTINA 3",{"EQUIPO":"GUILLOTINA 3","AREA":"ALMACÉN MP"}),
+    ("HOJEADORA",{"EQUIPO":"HOJEADORA","AREA":"ALMACÉN MP"}),
+    ("EMBOLSADORA",{"EQUIPO":"EMBOLSADORA","AREA":"MANUALES"}),
   ]
   libro_solicitudes = None
   ws_solicitudes = None
@@ -97,9 +98,13 @@ class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES(MANTENIMIENTO_PREVENTIVO_C
     return dict_solicitud
 
   def get_folio(self, fecha, equipo, consecutivo):
-    temp = dt.datetime(1899, 12, 30)    # Note, not 31st Dec but 30th!
+    temp = datetime(1899, 12, 30)    # Note, not 31st Dec but 30th!
     delta = fecha - temp
-    return float(delta.days) + (float(delta.seconds) / 86400)
+    fecha_numero = round(float(delta.days) + (float(delta.seconds) / 86400))
+    str_consecutivo = str(consecutivo)
+    str_consecutivo = "0"*(3 - len(str_consecutivo)) + str_consecutivo
+    return f"{fecha_numero}-{equipo}-{str_consecutivo}"
+    
   ############################################ EVENTOS ############################################
 
   def drop_down_area_change(self, **event_args):
@@ -123,17 +128,21 @@ class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES(MANTENIMIENTO_PREVENTIVO_C
     if respuesta == False:
       alert(title="ERROR!", content="Faltan campos por llenar!")
     else:
-      dict_datos = {
-        "id_solicitud_mtto":(max([int(item['id_solicitud_mtto']) for item in self.registros_solicitudes]) + 1) if len(self.registros_solicitudes) > 0 else 1,
-        "folio":"",
-        "id_usuario_registrador":"4",
-        "usuario_registrador":"test",
-        "operacion":"creacion",
-        "marca_temporal":datetime.now(),
-        "registro_principal":1
-      }
-      respuesta.update(dict_datos)
-      print(respuesta)
+      with Notification("Generando registro",title="REGISTRO.", style="info"):
+        id_nuevo_solicitud_mtto = (max([int(item['id_solicitud_mtto']) for item in self.registros_solicitudes]) + 1) if len(self.registros_solicitudes) > 0 else 1
+        dict_datos = {
+          "id_solicitud_mtto":id_nuevo_solicitud_mtto,
+          "folio":self.get_folio(datetime.now(),self.drop_down_equipo.selected_value['EQUIPO'],1) if len(self.registros_solicitudes) > 0 else self.get_folio(datetime.now(),self.drop_down_equipo.selected_value['EQUIPO'],id_nuevo_solicitud_mtto),
+          "id_usuario_registrador":"4",
+          "usuario_registrador":"test",
+          "operacion":"creacion",
+          "marca_temporal":datetime.now(),
+          "registro_principal":1
+        }
+        respuesta.update(dict_datos)
+      with Notification("Guardando registro en la base de datos...", title="GUARDANDO.", style="info"):
+        self.ws_solicitudes.add_row(**respuesta)
+      Notification("Registro de solicitud guardada correctamente.",title="GUARDADO.", style="success")
 
   def drop_down_equipo_change(self, **event_args):
     if self.drop_down_equipo.selected_value != None:
