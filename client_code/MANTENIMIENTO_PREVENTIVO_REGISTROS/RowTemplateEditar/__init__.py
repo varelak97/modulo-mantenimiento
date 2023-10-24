6d675119be2e80ef84aea67dda39eb4ab7887516
erp@ensel.org <erp@ensel.org>
2023-10-24 21:10:39 +0000

    Edited MANTENIMIENTO_PREVENTIVO_PROGRAMACION and MANTENIMIENTO_PREVENTIVO_REGISTROS.RowTemplateEditar

--- client_code/MANTENIMIENTO_PREVENTIVO_REGISTROS/RowTemplateEditar/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_REGISTROS/RowTemplateEditar/__init__.py
@@ -21,14 +21,10 @@ class RowTemplateEditar(RowTemplateEditarTemplate):
     respuesta = alert(title=self.label_equipo.text,buttons=botones)
     if respuesta == "checklist":
       datos['clave_form'] = 'MANTENIMIENTO_PREVENTIVO_CHECKLIST'
-      datos['id_mtto_preventivo'] = self.button_editar.tag
       datos['modo'] = "checklist"
-      self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo',datos=datos)
     elif respuesta == "ver_checklist":
       datos['clave_form'] = 'MANTENIMIENTO_PREVENTIVO_CHECKLIST'
-      datos['id_mtto_preventivo'] = self.button_editar.tag
       datos['modo'] = "ver_checklist"
-      self.parent.parent.parent.parent.parent.parent.raise_event('x-actualizar_form_activo',datos=datos)
     elif respuesta == "reprogramar":
       datos['clave_form'] = 'MANTENIMIENTO_PREVENTIVO_PROGRAMACION'
       datos['modo'] = "reprogramar"
