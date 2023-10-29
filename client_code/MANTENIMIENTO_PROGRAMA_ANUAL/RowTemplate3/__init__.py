c7798712f424a678583ff664707d1b6555e513a7
erp@ensel.org <erp@ensel.org>
2023-10-29 02:02:09 +0000

    Created MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplate2, MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplate3, MANTENIMIENTO_PROGRAMA_ANUAL_V1, MANTENIMIENTO_PROGRAMA_ANUAL_V1.RowTemplateDias and 1 more; Edited theme.css, settings and MANTENIMIENTO_PROGRAMA_ANUAL; Deleted MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplateDias, MANTENIMIENTO_PROGRAMA_ANUAL_V2, MANTENIMIENTO_PROGRAMA_ANUAL_V2.RowTemplate2 and MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplateDias.RowTemplateNotificaciones

--- client_code/MANTENIMIENTO_PROGRAMA_ANUAL/RowTemplate3/__init__.py
+++ client_code/MANTENIMIENTO_PROGRAMA_ANUAL/RowTemplate3/__init__.py
@@ -1,11 +1,15 @@
-from ._anvil_designer import MANTENIMIENTO_PROGRAMA_ANUAL_V2Template
+from ._anvil_designer import RowTemplate3Template
 from anvil import *
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 
-class MANTENIMIENTO_PROGRAMA_ANUAL_V2(MANTENIMIENTO_PROGRAMA_ANUAL_V2Template):
+class RowTemplate3(RowTemplate3Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def link_1_click(self, **event_args):
+    objeto_link = self.get_components()
+    objeto_link[0].background = app.theme_colors['Primary']
