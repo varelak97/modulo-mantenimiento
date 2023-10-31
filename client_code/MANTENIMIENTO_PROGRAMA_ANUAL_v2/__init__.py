c0d75f4709e52848ba075ab5347858899c70e0c6
erp@ensel.org <erp@ensel.org>
2023-10-31 13:57:08 +0000

    Created MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplateDias, MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplateDias.RowTemplateNotificaciones, MANTENIMIENTO_PROGRAMA_ANUAL_v2, MANTENIMIENTO_PROGRAMA_ANUAL_v2.RowTemplate2 and 1 more; Edited MANTENIMIENTO_PROGRAMA_ANUAL; Deleted MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplate2, MANTENIMIENTO_PROGRAMA_ANUAL.RowTemplate3, MANTENIMIENTO_PROGRAMA_ANUAL_V1, MANTENIMIENTO_PROGRAMA_ANUAL_V1.RowTemplateDias and 1 more

---
+++ client_code/MANTENIMIENTO_PROGRAMA_ANUAL_v2/__init__.py
@@ -0,0 +1,24 @@
+from ._anvil_designer import MANTENIMIENTO_PROGRAMA_ANUAL_v2Template
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+
+class MANTENIMIENTO_PROGRAMA_ANUAL_v2(MANTENIMIENTO_PROGRAMA_ANUAL_v2Template):
+  #################################### DEFINICION DE VARIABLES ####################################
+  datos = {}
+  def __init__(self, datos, **properties):
+    ######################## CARGA DE DATOS E INICIALIZACION DE VARIABLES #########################
+    self.init_components(**properties)
+    self.datos = datos
+    self.test()
+    # Any code you write here will run before the form opens.
+  ################################ FUNCIONES PERSONALIZADS ########################################
+  def test(self):
+    """columnas = self.data_grid_1.columns
+    print(columnas[0])
+    columnas.pop(0)
+    self.data_grid_1.columns = columnas"""
+    self.repeating_panel_mes.items = [1,2,3,4]
+    pass
+
+  ############################################ EVENTOS ############################################
