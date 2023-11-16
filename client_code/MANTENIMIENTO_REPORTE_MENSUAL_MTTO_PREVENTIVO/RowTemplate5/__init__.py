d84124acb1d9dffb4028ed23b8910c93fa91eb9b
erp@ensel.org <erp@ensel.org>
2023-11-16 23:00:47 +0000

    Created MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO.RowTemplate6 and MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO.RowTemplate5; Edited MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO

--- client_code/MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO/RowTemplate5/__init__.py
+++ client_code/MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO/RowTemplate5/__init__.py
@@ -1,10 +1,9 @@
-from ._anvil_designer import MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVOTemplate
+from ._anvil_designer import RowTemplate5Template
 from anvil import *
-import plotly.graph_objects as go
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 
-class MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVO(MANTENIMIENTO_REPORTE_MENSUAL_MTTO_PREVENTIVOTemplate):
+class RowTemplate5(RowTemplate5Template):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
