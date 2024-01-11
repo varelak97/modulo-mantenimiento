862722c077db92a2abb8a7e1f82c252ece5ef97f
erp@ensel.org <erp@ensel.org>
2024-01-11 13:53:41 +0000

    Edited MANTENIMIENTO_PROGRAMA_ANUAL

--- client_code/MANTENIMIENTO_PROGRAMA_ANUAL/__init__.py
+++ client_code/MANTENIMIENTO_PROGRAMA_ANUAL/__init__.py
@@ -176,7 +176,7 @@ class MANTENIMIENTO_PROGRAMA_ANUAL(MANTENIMIENTO_PROGRAMA_ANUALTemplate):
       #filtra por datos del mes
       if mes_prog == mes and  int(fecha.year) == int(anio):
         self.fill_indicador(dia_prog, indicadores_mtto_mes, item)
-    print(f"los indicadores de mtto:{indicadores_mtto_mes}")
+    #print(f"los indicadores de mtto:{indicadores_mtto_mes}")
     self.card_calendario.visible = False
     mes_calendario = calendar.month(int(anio),mes)[0:-1] #Se descarta el último salto de línea, pues en caso de haber 6 semanas, se toma una 7a inexistente
 
