401398d287b48fc68e446770b9a0c27dca75f176
erp@ensel.org <erp@ensel.org>
2024-01-16 20:18:37 +0000

    Created MANTENIMIENTO_INICIO_SESION

---
+++ client_code/MANTENIMIENTO_INICIO_SESION/__init__.py
@@ -0,0 +1,11 @@
+from ._anvil_designer import MANTENIMIENTO_INICIO_SESIONTemplate
+from anvil import *
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+
+class MANTENIMIENTO_INICIO_SESION(MANTENIMIENTO_INICIO_SESIONTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
