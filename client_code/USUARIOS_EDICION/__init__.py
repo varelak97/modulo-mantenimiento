693b86f4c3ce181cba2e90217a221850d6e7865a
erp@ensel.org <erp@ensel.org>
2024-01-16 22:59:41 +0000

    Created USUARIOS_EDICION

---
+++ client_code/USUARIOS_EDICION/__init__.py
@@ -0,0 +1,12 @@
+from ._anvil_designer import USUARIOS_EDICIONTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+
+class USUARIOS_EDICION(USUARIOS_EDICIONTemplate):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
