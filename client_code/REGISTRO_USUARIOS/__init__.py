89cf6ea219df3d4a5344b3a3a63d39d7b17bc704
erp@ensel.org <erp@ensel.org>
2024-01-31 22:30:50 +0000

    Edited REGISTRO_USUARIOS and MANTENIMIENTO_INICIO_SESION

--- client_code/REGISTRO_USUARIOS/__init__.py
+++ client_code/REGISTRO_USUARIOS/__init__.py
@@ -7,8 +7,8 @@ from anvil.google.drive import app_files
 class REGISTRO_USUARIOS(REGISTRO_USUARIOSTemplate):
   ##################################### VARIABLES #####################################
   datos = {}
-  libro_usuarios = app_files.usuarios_erp
-  ws_usuarios = libro_usuarios['Vista']
+  #libro_usuarios = app_files.usuarios_erp
+  #ws_usuarios = libro_usuarios['Vista']
   usuarios = None
   def __init__(self, datos, **properties):
     # Set Form properties and Data Bindings.
@@ -21,7 +21,8 @@ class REGISTRO_USUARIOS(REGISTRO_USUARIOSTemplate):
     if self.text_box_password.text != self.text_box_confirm_password.text:
       alert("Las contraseñas no coinciden.", title="ERROR AL REGISTRAR")
     else:
-      self.usuarios = self.ws_usuarios.rows
+      pass
+      """self.usuarios = self.ws_usuarios.rows
       for usuario in self.usuarios:
-        if usuario[]
+        if usuario[]"""
       
