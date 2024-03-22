05e5d7058844528e01c8568bfe305d4ad4c2e2e8
erp@ensel.org <erp@ensel.org>
2024-03-22 19:06:23 +0000

    Edited MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES

--- client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
+++ client_code/MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES/__init__.py
@@ -24,6 +24,7 @@ class MANTENIMIENTO_PREVENTIVO_CORRECTIVO_SOLICITUDES(MANTENIMIENTO_PREVENTIVO_C
   
   def __init__(self, datos, **properties):
     self.init_components(**properties)
+    print(datos)
     
     ######################## CARGA DE DATOS E INICIALIZACION DE VARIABLES #########################
     self.datos = datos
