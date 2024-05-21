8759a129669e49fdbc2d704fe1607f242200b664
erp@ensel.org <erp@ensel.org>
2024-05-21 22:16:19 +0000

    Created MANTENIMIENTO_CONTROL_HERRAMENTALES.RowTemplate12; Edited MANTENIMIENTO_CONTROL_HERRAMENTALES and MANTENIMIENTO_LISTA_EQUIPOS

---
+++ client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/RowTemplate12/__init__.py
@@ -0,0 +1,13 @@
+from ._anvil_designer import RowTemplate12Template
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+
+
+class RowTemplate12(RowTemplate12Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+
+    # Any code you write here will run before the form opens.
