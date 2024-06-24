b7d7d25ec6e062fcbf1e8ed1c662e83c76e49c2e
erp@ensel.org <erp@ensel.org>
2024-06-24 14:57:25 +0000

    Edited MANTENIMIENTO_CONTROL_HERRAMENTALES.Form_Edicion_Herramental

--- client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
@@ -8,10 +8,24 @@ from anvil.google.drive import app_files
 class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
   lista_componentes = None
   datos = None
+  ws_control_herramentales = None
+  ss_vista_numeros_parte = None
+  numeros_parte = None
+  ss_registros = None
+  registros = None
+  
   def __init__(self, datos, **properties):
-    # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
+    self.datos = datos
+    self.ws_control_herramentales = app_files.control_herramentales
+    self.ss_vista_numeros_parte = self.ws_control_herramentales['VISTA_NUMEROS_PARTE']
+    self.registros = self.ss_vista_numeros_parte.rows
+    self.ss_registros = self.ws_control_herramentales['VISTA_REGISTROS']
+    self.registros = self.ss_registros.rows
+
   ################################################### FUNCIONES PERSONALIZADAS ###################################################
+  def get_data(self):
+    pass
 
   ############################################################ EVENTOS ###########################################################
