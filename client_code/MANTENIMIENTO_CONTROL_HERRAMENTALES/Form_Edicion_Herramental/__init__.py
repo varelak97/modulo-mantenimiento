c6042f9ceed9bd4a113ac175eea396ad32027ff6
erp@ensel.org <erp@ensel.org>
2024-06-28 17:53:47 +0000

    Edited MANTENIMIENTO_CONTROL_HERRAMENTALES and MANTENIMIENTO_CONTROL_HERRAMENTALES.Form_Edicion_Herramental

--- client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
@@ -14,10 +14,11 @@ class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
   herramentales = None
   lista_componentes = None
   registro_actual = None
+  campos_no_obligatorios = []
   
   def __init__(self, datos, **properties):
     self.init_components(**properties)
-    self.set_ini_config()
+    self.set_ini_config(datos)
 
     if self.datos['modo'] == "edicion":
       self.registro_actual = Funciones_Globales.get_registro(self.datos['id_herramental'], 'id_herramental', self.herramentales)
@@ -28,6 +29,7 @@ class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
   def set_ini_config(self, datos):
     self.ws_herramentales = app_files.control_herramentales
     self.ss_heramentales = self.ws_herramentales['HERRAMENTALES']
+    self.herramentales = self.ss_heramentales.rows
     self.datos = datos
 
     self.lista_componentes = [
@@ -36,36 +38,61 @@ class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
       self.text_box_tipo_material,
       self.text_box_tipo_suaje,
       self.text_box_ubicacion,
-      self.text_box_vida_util
+      self.text_box_vida_util,
+      self.text_box_alerta
     ]
 
   def save_data(self, modo):
     mensaje = "Actualizando registro en la base de datos..." if modo == "edicion" else "Guardando registro en la base de datos..."
     title = "ACTUALIZANDO." if modo == "edicion" else "GUARDANDO."
-    comentarios = "Alta" if modo == "nuevo" else "Edición"
     with Notification(mensaje, title=title, style="notification"):
       if modo == "edicion":
         self.registro_actual['registro_principal'] = 0
+        
       dicc_datos = Funciones_Globales.genera_diccionario(self.lista_componentes)
+      if modo == "nuevo":
+        dicc_datos['contador'] = 0
+        dicc_datos['activo'] = 1
+        
       dicc_datos['id_herramental'] = (max([int(item['id_herramental']) for item in self.herramentales]) + 1) if len(self.herramentales) > 0 else 0
       dicc_datos['registro_principal'] = 1
       dicc_datos['id_usuario_registrador'] = self.datos['id_usuario_erp']
-      dicc_datos['comentarios'] = comentarios
+      dicc_datos['comentarios'] = "Alta" if modo == "nuevo" else "Edición"
       dicc_datos['marca_temporal'] = datetime.now()
       
       status = "registro_actualizado" if  modo == "edicion" else "registro_guardado"
-      self.ss_registros.add_row(**dicc_datos)
+      self.ss_heramentales.add_row(**dicc_datos)
       self.raise_event("x-close-alert",value=status)
 
   
   ############################################################ EVENTOS ############################################################
   def button_guardar_click(self, **event_args):
-    status = Funciones_Globales.validar_campos( self.lista_componentes, self.registro_actual, self.datos['modo'])
+    status = Funciones_Globales.validar_campos( self.lista_componentes, self.registro_actual, self.campos_no_obligatorios, self.datos['modo'])
     if status == 1:
+      alert(f"modo:{self.datos['modo']}")
       self.save_data(self.datos['modo'])
     elif status == 2:
       alert("No hay cambios que guardar.", title="ERROR!")
     elif status == 3:
       alert("faltan campos por llenar!", title="ERROR!")
+
+  def text_box_codigo_herramental_change(self, **event_args):
+    self.text_box_codigo_herramental.role = "outlined"
+
+  def text_area_descripcion_change(self, **event_args):
+    self.text_area_descripcion.role = "outlined"
+
+  def text_box_tipo_suaje_change(self, **event_args):
+    self.text_box_tipo_suaje.role = "outlined"
+
+  def text_box_vida_util_change(self, **event_args):
+    self.text_box_vida_util.role = "outlined"
+
+  def text_box_ubicacion_change(self, **event_args):
+    self.text_box_ubicacion.role = "outlined"
+
+  def text_box_tipo_material_change(self, **event_args):
+    self.text_box_tipo_material.role = "outlined"
+
 
     
--- client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/__init__.py
@@ -13,36 +13,48 @@ class MANTENIMIENTO_CONTROL_HERRAMENTALES(MANTENIMIENTO_CONTROL_HERRAMENTALESTem
   ws_herramentales = None
   ss_herramentales = None
   herramentales = None
+  
   def __init__(self, datos, **properties):
     self.init_components(**properties)
-    self.datos = datos
-
+    self.set_ini_config(datos)
     self.set_event_handler('x-abrir_form', self.abrir_popup_form)
     
-    self.ws_herramentales = app_files.control_herramentales
-    self.ss_herramentales = self.ws_herramentales['VISTA_HERRAMENTALES']
-
     self.button_actualizar_click()
 
   ############################################# FUNCIONES PERSONALIZADAS ##############################################
+  def set_ini_config(self, datos):
+    self.datos = datos
+    self.ws_herramentales = app_files.control_herramentales
+    self.ss_herramentales = self.ws_herramentales['VISTA_HERRAMENTALES']
+
+  def get_datos(self):
+    self.herramentales = self.ss_herramentales.rows
+    self.repeating_panel_herramentales.items = self.herramentales
+    
   def abrir_popup_form(self, datos, **event_args):
     #datos['id_usuario_erp'] = self.datos['id_usuario_erp']
     if datos['clave_form'] == 'REGISTROS_HERRAMENTAL':
-      datos.update(self.datos)
+      datos['id_usuario_erp'] = self.datos['id_usuario_erp']
       self.abrir_form(MANTENIMIENTO_REGISTRO_SUAJES(datos))
     elif datos['clave_form'] == 'FORM_HERRAMENTAL':
-      datos.update(self.datos)
+      datos['id_usuario_erp'] = self.datos['id_usuario_erp']
       self.abrir_form(Form_Edicion_Herramental(datos))
       
   def abrir_form(self, form_de_interes):
     respuesta = alert(content = form_de_interes, large=True, dismissible=False, buttons=[("REGRESAR", True)], role="wide-modal-content-bigger")
-    if respuesta == "registro_guardado":
-        self.button_actualizar_click()
+    if respuesta is not False and  respuesta is not None:
+      mensaje = "El registro ha sido guardado correctamente" if respuesta == "registro_guardado" else "El registro ha sido actualizado correctamente."
+      Notification(mensaje, title="ÉXITO!", style="success").show(3)
+      self.button_actualizar_click()
   
   ###################################################### EVENTOS ######################################################
   def button_actualizar_click(self, **event_args):
-    self.herramentales = self.ss_herramentales.rows
-    self.repeating_panel_herramentales.items = self.herramentales
+    if len(event_args) > 0:
+      with Notification("Actualizando tabla...", title="ACTUALIZANDO.", style="notification"):
+        self.get_datos()
+    else:
+      self.get_datos()
+    
 
   def button_nuevo_click(self, **event_args):
     datos = {}
