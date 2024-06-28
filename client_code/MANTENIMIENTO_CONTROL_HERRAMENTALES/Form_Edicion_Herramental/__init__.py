67a2a40400e531a1ea6cf0cc272c795189d72097
erp@ensel.org <erp@ensel.org>
2024-06-28 16:31:48 +0000

    Edited MANTENIMIENTO_CONTROL_HERRAMENTALES.Form_Edicion_Herramental

--- client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_HERRAMENTALES/Form_Edicion_Herramental/__init__.py
@@ -4,6 +4,7 @@ import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 from ... import Funciones_Globales
+from datetime import datetime
 
 
 class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
@@ -38,30 +39,30 @@ class Form_Edicion_Herramental(Form_Edicion_HerramentalTemplate):
       self.text_box_vida_util
     ]
 
-  def button_guardar_click(self, **event_args):
-    status = Funciones_Globales.validar_campos( self.lista_componentes, None, self.datos['modo'])
-    if status == 1:
-      mensaje = "Actualizando registro en la base de datos..." if self.datos['modo'] == "edicion" else "Guardando registro en la base de datos..."
-      title = "ACTUALIZANDO." if self.datos['modo'] == "edicion" else "GUARDANDO."
-      with Notification(mensaje, title=title):
-        dicc_datos = Funciones_Globales.genera_diccionario(self.lista_input_components)
-        dicc_datos['id_numero_parte'] = dicc_datos['id_numero_parte'][0]
-        dicc_datos['id_herramental'] = dicc_datos['tipo_suaje'][1]
-        dicc_datos['tipo_suaje'] = dicc_datos['tipo_suaje'][0]
-        dicc_datos['id_registro'] = (max([int(item['id_registro']) for item in self.registros]) + 1) if len(self.registros) > 0 else 0
-        dicc_datos['status'] = 0
-        dicc_datos['registro_principal'] = 1
-        dicc_datos['id_usuario_registrador'] = self.datos['id_usuario_erp']
-        dicc_datos['comentarios'] = "Alta"
-        dicc_datos['marca_temporal'] = datetime.now()
-        status = ""
-        if self.datos['modo'] == "edicion":
-          #falta codigo cuando se actualiza
-          status = "registro_actualizado"
-        elif self.datos['modo'] == "nuevo":
-          self.ss_registros.add_row(**dicc_datos)
-          status = "registro_guardado"
+  def save_data(self, modo):
+    mensaje = "Actualizando registro en la base de datos..." if modo == "edicion" else "Guardando registro en la base de datos..."
+    title = "ACTUALIZANDO." if modo == "edicion" else "GUARDANDO."
+    comentarios = "Alta" if modo == "nuevo" else "Edición"
+    with Notification(mensaje, title=title, style="notification"):
+      if modo == "edicion":
+        self.registro_actual['registro_principal'] = 0
+      dicc_datos = Funciones_Globales.genera_diccionario(self.lista_componentes)
+      dicc_datos['id_herramental'] = (max([int(item['id_herramental']) for item in self.herramentales]) + 1) if len(self.herramentales) > 0 else 0
+      dicc_datos['registro_principal'] = 1
+      dicc_datos['id_usuario_registrador'] = self.datos['id_usuario_erp']
+      dicc_datos['comentarios'] = comentarios
+      dicc_datos['marca_temporal'] = datetime.now()
+      
+      status = "registro_actualizado" if  modo == "edicion" else "registro_guardado"
+      self.ss_registros.add_row(**dicc_datos)
       self.raise_event("x-close-alert",value=status)
+
+  
+  ############################################################ EVENTOS ############################################################
+  def button_guardar_click(self, **event_args):
+    status = Funciones_Globales.validar_campos( self.lista_componentes, self.registro_actual, self.datos['modo'])
+    if status == 1:
+      self.save_data(self.datos['modo'])
     elif status == 2:
       alert("No hay cambios que guardar.", title="ERROR!")
     elif status == 3:
