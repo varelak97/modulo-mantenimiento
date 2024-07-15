2226380b219228fa6fa8d3f0baaed44a615d1d09
erp@ensel.org <erp@ensel.org>
2024-07-15 22:07:50 +0000

    Edited MANTENIMIENTO_NUMEROS_PARTE.Form_Edicion_Numero_Parte

--- client_code/MANTENIMIENTO_NUMEROS_PARTE/Form_Edicion_Numero_Parte/__init__.py
+++ client_code/MANTENIMIENTO_NUMEROS_PARTE/Form_Edicion_Numero_Parte/__init__.py
@@ -115,6 +115,10 @@ class Form_Edicion_Numero_Parte(Form_Edicion_Numero_ParteTemplate):
   ########################################################## EVENTOS #########################################################
   def button_agregar_click(self, **event_args):
     dropdown_suajes = DropDown(role='outlined', background='On Primary', placeholder='-- SELECCIONE --', items=self.lista_suajes)
+    if self.drop_down_cliente.selected_value is None:
+      pass
+    else:
+      pass
     respuesta = alert(dropdown_suajes, title="SELECCIONE SUAJE:", buttons=[("ACEPTAR", True),("CANCELAR", False)])
     if respuesta:
       self.drop_down_cliente.enabled = False
