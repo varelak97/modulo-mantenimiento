8b7c85efe0957ce2135b56c4e6cc4f4f55eb7456
erp@ensel.org <erp@ensel.org>
2024-08-28 17:58:46 +0000

    Created MANTENIMIENTO_CONTROL_SUAJES.opciones_inspecciones; Edited MANTENIMIENTO_CONTROL_SUAJES.RowTemplateHerramental

---
+++ client_code/MANTENIMIENTO_CONTROL_SUAJES/opciones_inspecciones/__init__.py
@@ -0,0 +1,31 @@
+from ._anvil_designer import opciones_inspeccionesTemplate
+from anvil import *
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+
+
+class opciones_inspecciones(opciones_inspeccionesTemplate):
+  id_inspeccion = None
+  modo = None
+  def __init__(self, id_inspeccion, modo, **properties):
+    self.init_components(**properties)
+    self.id_inspeccion = id_inspeccion
+    self.modo = modo
+
+  ################################################# FUNCIONES PERSONALIZADAS #################################################
+  def abrir_form(self, modo):
+    datos  = {
+      'id_inspeccion': self.id_inspeccion,
+      'modo': modo,
+      'clave_form': 'FORM_INSPECCION_SUAJE'
+    }
+    self.popper.pop("hide")
+    self.popper.parent.parent.parent.parent.parent.parent.raise_event('x-abrir_form', datos=datos)
+
+  ########################################################## EVENTOS #########################################################
+  def button_editar_click(self, **event_args):
+    self.abrir_form('edicion')
+
+  def button_ver_click(self, **event_args):
+    self.abrir_form('visor')
