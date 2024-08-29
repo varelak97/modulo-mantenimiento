d54a40d1ae2ecb6aff54aee5cc56dac600df94f4
erp@ensel.org <erp@ensel.org>
2024-08-29 21:18:38 +0000

    Edited MANTENIMIENTO_CONTROL_SUAJES.opciones_inspecciones

--- client_code/MANTENIMIENTO_CONTROL_SUAJES/opciones_inspecciones/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_SUAJES/opciones_inspecciones/__init__.py
@@ -38,7 +38,10 @@ class opciones_inspecciones(opciones_inspeccionesTemplate):
 
   def button_visual_editar_click(self, **event_args):
     if self.status_reportes[0] == '1' or self.status_reportes[1] == '1':
-      self.abrir_form('edicion', 'FORM_INSPECCION_SUAJE')
+      if self.status_reportes[0] == '1':
+        self.abrir_form('edicion', 'FORM_INSPECCION_VISUAL')
+      elif self.status_reportes[1] == '0':
+        self.abrir_form('edicion', 'FORM_INSPECCION_DIMENSIONAL')
     else:
       self.abrir_form('nuevo')
 
