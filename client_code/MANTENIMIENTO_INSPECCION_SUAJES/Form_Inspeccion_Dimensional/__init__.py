fdf59fcddd0249cfa2b35204805974a8ca3e4811
erp@ensel.org <erp@ensel.org>
2024-09-23 15:46:21 +0000

    Edited MANTENIMIENTO_INSPECCION_SUAJES.Form_Inspeccion_Dimensional and MANTENIMIENTO_CONTROL_SUAJES

--- client_code/MANTENIMIENTO_INSPECCION_SUAJES/Form_Inspeccion_Dimensional/__init__.py
+++ client_code/MANTENIMIENTO_INSPECCION_SUAJES/Form_Inspeccion_Dimensional/__init__.py
@@ -9,9 +9,19 @@ class Form_Inspeccion_Dimensional(Form_Inspeccion_DimensionalTemplate):
   ws_herramentales = None
   ss_reporte_suajes = None
   reporte_suajes = None
-  ss_vista_suaje = None
-  vista_suaje = None
+  ss_vista_clientes = None
+  vista_clientes = None
+  ss_vista_suajes = None
+  vista_suajes = None
+  ss_suajes = None
+  suajes = None
+  registro_actual = {}
   datos = None
+  modos_botones = None
+  status_botones = None
+  lista_componentes = None
+  lista_componentes_validacion = None
+  campos_no_obligatorios = None
 
   def __init__(self, datos, **properties):
     self.init_components(**properties)
@@ -22,8 +32,49 @@ class Form_Inspeccion_Dimensional(Form_Inspeccion_DimensionalTemplate):
   def set_ini_config(self, datos):
     self.datos = datos
     self.ws_herramentales = app_files.control_herramentales
-    self.ss_reporte_suajes = self.ws_herramentales["REVISION_SUAJE"]
-    self.ss_vista_suaje = self.ws_herramentales["VISTA_HERRAMENTALES"]
+    self.ss_reporte_suajes = self.ws_herramentales['REVISION_SUAJES']
+    self.ss_vista_clientes = self.ws_herramentales['VISTA_CLIENTES']
+    self.ss_vista_suajes = self.ws_herramentales['VISTA_HERRAMENTALES']
+    self.ss_suajes = self.ws_herramentales['HERRAMENTALES']
+    
+    self.lista_componentes = [
+      self.text_box_cliente,
+      self.text_box_codigo_suaje,
+      self.text_box_tipo_suaje,
+      self.text_area_descripcion,
+      self.text_box_revisor,
+      self.text_area_filo,
+      self.text_area_union,
+      self.text_area_estado,
+      self.button_estado_bien,
+      self.button_estado_mal,
+      self.button_filo_bien,
+      self.button_filo_mal,
+      self.button_union_bien,
+      self.button_union_mal
+    ]
+    self.lista_componentes_validacion = [
+      self.text_area_filo,
+      self.text_area_union,
+      self.text_area_estado,
+      self.button_filo_bien,
+      self.button_union_bien,
+      self.button_estado_bien
+    ]
+    self.modos_botones = [
+        {'tag':'filo_bien','modo':True,'llave':'status_filo'},
+        {'tag':'union_bien','modo':True,'llave':'status_union'},
+        {'tag':'estado_bien','modo':True,'llave':'status_estado'},
+        {'tag':'filo_mal','modo':False,'llave':'status_filo'},
+        {'tag':'union_mal','modo':False,'llave':'status_union'},
+        {'tag':'estado_mal','modo':False,'llave':'status_estado'}
+      ]
+    self.status_botones = [
+      {'tag':"filo_bien", "valor": None, 'llave':'status_filo'},
+      {'tag':"union_bien", "valor": None, 'llave':'status_union'},
+      {'tag':"estado_bien", "valor": None, 'llave':'status_estado'}
+    ]
+    self.campos_no_obligatorios = ["comentarios_filo", "comentarios_union", "comentarios_estado"]
 
   def get_datos(self):
     if self.datos["modo"] == "edicion":
