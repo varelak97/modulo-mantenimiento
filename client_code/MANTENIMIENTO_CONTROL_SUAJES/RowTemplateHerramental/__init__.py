2b81557c540583225afdf4972c20edfb5b4433d6
erp@ensel.org <erp@ensel.org>
2024-10-04 13:34:17 +0000

    Edited MANTENIMIENTO_CONTROL_SUAJES.RowTemplateHerramental

--- client_code/MANTENIMIENTO_CONTROL_SUAJES/RowTemplateHerramental/__init__.py
+++ client_code/MANTENIMIENTO_CONTROL_SUAJES/RowTemplateHerramental/__init__.py
@@ -12,19 +12,6 @@ class RowTemplateHerramental(RowTemplateHerramentalTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    if int(self.item['contador']) >= int(self.item['vida_util']):
-      self.label_alerta.icon = "fa:exclamation-circle"
-      self.label_alerta.background = app.theme_colors['Red']
-      self.label_alerta.foreground = app.theme_colors['Yellow']
-    elif int(self.item['contador']) >= int(self.item['alerta']):
-      self.label_alerta.icon = "fa:warning"
-      self.label_alerta.foreground = app.theme_colors['Primary']
-      self.label_alerta.background = app.theme_colors['Yellow']
-    else:
-      self.label_alerta.icon = "fa:check"
-      self.label_alerta.foreground = app.theme_colors['Primary']
-      self.label_alerta.background = app.theme_colors['SecondaryGreen']
-
     datos = {
       'id_herramental': self.item['id_herramental'],
       'id_inspeccion': self.item['id_inspeccion'],
@@ -38,8 +25,21 @@ class RowTemplateHerramental(RowTemplateHerramentalTemplate):
       'contador': self.item['contador'],
       'vida_util':self.item['vida_util']
     }
+
     if int(self.item['contador']) >= int(self.item['vida_util']):
+      self.label_alerta.icon = "fa:exclamation-circle"
+      self.label_alerta.background = app.theme_colors['Red']
+      self.label_alerta.foreground = app.theme_colors['Yellow']
       self.link_alerta.popover(content=opciones_inspecciones(datos), title="REPORTES DE INSPECCIÓN", trigger="click", max_width="700px")
+    elif int(self.item['contador']) >= int(self.item['alerta']):
+      self.label_alerta.icon = "fa:warning"
+      self.label_alerta.foreground = app.theme_colors['Primary']
+      self.label_alerta.background = app.theme_colors['Yellow']
+    else:
+      self.label_alerta.icon = "fa:check"
+      self.label_alerta.foreground = app.theme_colors['Primary']
+      self.label_alerta.background = app.theme_colors['SecondaryGreen']
+    #if int(self.item['contador']) >= int(self.item['vida_util']):
     self.button_editar.popover(content=opciones_herramentales(self.button_editar.tag),title=self.label_codigo_herramental.text, trigger="click",max_width="450px")
 
   def button_ver_click(self, **event_args):
