ddc9dc693fcd451d602e3471af0f7be4e73c46e1
erp@ensel.org <erp@ensel.org>
2024-10-09 16:15:35 +0000

    Created iconos/SISTEMAS.png; Edited MANTENIMIENTO_MENU

--- client_code/MANTENIMIENTO_MENU/__init__.py
+++ client_code/MANTENIMIENTO_MENU/__init__.py
@@ -3,11 +3,30 @@ from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
-
+from anvil_extras import augment
 
 class MANTENIMIENTO_MENU(MANTENIMIENTO_MENUTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
-    # Any code you write here will run before the form opens.
+    augment.set_event_handler(self.outlined_card_mantenimiento,'mouseenter',self.set_color)
+    augment.set_event_handler(self.outlined_card_mantenimiento,'mouseleave',self.set_color)
+    augment.set_event_handler(self.outlined_card_sistemas,'mouseenter',self.set_color)
+    augment.set_event_handler(self.outlined_card_sistemas,'mouseleave',self.set_color)
+
+  ############################################################# FUNCIONES PERSONALIZADAS #############################################################
+  def set_color(self,**event_args):
+    card = event_args['sender']
+    if 'enter' in event_args['event_type']:
+      card.background = app.theme_colors['LightBlue']
+    else:
+      card.background = app.theme_colors['Background']
+
+  def link_mantenimiento_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
+
+  def link_solicitudes_mtto_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    pass
