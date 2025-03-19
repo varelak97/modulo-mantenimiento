876a84e852079b88d7ac7c51c317074559ca5e93
erp@ensel.org <erp@ensel.org>
2025-03-19 15:25:46 +0000

    Edited creacion_pdf

--- server_code/creacion_pdf.py
+++ server_code/creacion_pdf.py
@@ -7,12 +7,13 @@ import anvil.pdf
 import requests
 import json
 
-url_google_script = "https://script.google.com/macros/s/AKfycbxR6VJ4uCp_sQUDTRlfQgzj9S4ITNQnD6WB2hSGbGWXsj8EgkYu5dntihFUo-gA37M/exec"
+url_google_script = "https://script.google.com/macros/s/AKfycbyLn0NuBYA3934yIZP0oGbC6sFDhcEawBcH8YTKTeJbHMMUOazR02nGFb-0_-mXHHw/exec"
 
 @anvil.server.callable
 def crear_pdf(datos):
-  respuesta =  json.loads(requests.post(url_google_script, data=datos).text)['respuesta']
-  return respuesta
+  #respuesta =  json.loads(requests.post(url_google_script, data=datos).text)['respuesta']
+  respuesta = requests.post(url_google_script, data=datos)
+  return respuesta.json()
   
 """def crear_pdf(datos):
   media_object = anvil.pdf.render_form('MANTENIMIENTO_PREVENTIVO_CHECKLIST', datos)
