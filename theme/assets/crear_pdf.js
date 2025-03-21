5201b5d9142aef7aec62fd63686908059cbed5c4
erp@ensel.org <erp@ensel.org>
2025-03-21 17:42:33 +0000

    Edited standard-page.html and crear_pdf.js

--- theme/assets/crear_pdf.js
+++ theme/assets/crear_pdf.js
@@ -1,23 +1,23 @@
-function  generar_pdf(datos){
-  const { jsPDF } = window.jspdf;
-  const doc = new jsPDF();
+/*function  generar_pdf(datos){
+  var { jsPDF } = window.jspdf;
+  var doc = new jsPDF();
   
-  /*const encabezados = [["Nombre", "Edad", "Profesión"]];
-  const cuerpo = [["alfredo_new","22","ingeniero"],["jorge","33","licenciado"]];*/
+  const encabezados = [["Nombre", "Edad", "Profesión"]];
+  const cuerpo = [["alfredo_new","22","ingeniero"],["jorge","33","licenciado"]];
 
   
-  /*doc.text(datos['nombre'],20,40);*/
+  doc.text(datos['nombre'],20,40);
 
   // Generar tabla
-  /*doc.autoTable({
+  doc.autoTable({
       head: encabezados,
       body: cuerpo,
       startY: 20,
-  });*/
+  });
 
   doc.text("Nombre del personal que realizó el mantenimiento:",10,10);
 
   // Descargar el PDF
-  doc.save("test.pdf");
+  doc.save("test2.pdf");
   
-}
+}*/
